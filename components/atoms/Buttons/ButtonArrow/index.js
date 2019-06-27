3e94c35e158b2afbc7260aff9974dce7b2fc3509
Olya <olga.krasnova@flatstack.com>
2019-06-27 14:08:46 +0300

    add newsletter subscription form

--- components/atoms/Buttons/ButtonArrow/index.js
+++ components/atoms/Buttons/ButtonArrow/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as ArrowRight } from '../../../../static/images/icons/arrow_right.svg';
+
+const Button = styled.button`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  font-size: .8125rem;
+  padding: .9375rem 1.25rem .8125rem;
+  line-height: 1.2;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  overflow: hidden;
+  text-transform: uppercase;
+  border: none;
+  font-weight: 600;
+  outline: 0;
+  text-align: left;
+  background: linear-gradient(to right, #a30d10 50%, #d21115 50%);
+  background-size: 300% 150%;
+  background-position: right 0 bottom 0;
+  
+  &:hover {
+    background-position: left 0 bottom 0;
+  }
+  
+  svg {
+    width: 30px;
+  }
+`;
 
 const ButtonArrow = () => (
-  <button></button>
+  <Button>
+    <ArrowRight />
+  </Button>
 );
 
 export default ButtonArrow;
--- components/molecules/Forms/NewsletterSubscription/index.js
+++ components/molecules/Forms/NewsletterSubscription/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Input from '../../../atoms/Forms/Input';
+import ButtonArrow from '../../../atoms/Buttons/ButtonArrow';
+
+const Wrapper = styled.section`
+  background-color: #f3f3f3;
+  text-align: center;
+  padding: 80px 20px;
+`;
+
+const Title = styled.h5``;
+
+const LetterForm = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
+  max-width: 25rem;
+  margin: 20px auto 0;
+  
+  input {
+    border-radius: 0;
+    border-color: #000;
+    margin-right: 10px;
+    background-color: transparent;
+    border-right: 0;
+  }
+`;
 
 const NewsletterSubscription = () => (
-  <div></div>
+  <Wrapper>
+    <Title>Join our mailing list.</Title>
+    <LetterForm>
+      <Input type="email" name="email" placeholder="Your email address" />
+      <ButtonArrow />
+    </LetterForm>
+  </Wrapper>
 );
 
 export default NewsletterSubscription;
